f4c3b2c403d47204e89d09471b68c9457dfc7381
Fefeprogrammer <50031002+Fefeprogrammer@users.noreply.github.com>
2024-05-30 16:11:47 -0300

    Melhoria Layout
    Mais alterações no layout

--- js/script.js
+++ js/script.js
@@ -1,8 +1,53 @@
-const img = document.getElementById('img')
+window.onload = function(){
+    carregarPagina()
+}
 
-img.addEventListener('mouseover', entrar)
-img.addEventListener('mouseout', sair)
+function carregarPagina(){
+    const language = window.navigator.language;
+    console.log(language);
+    preencherTextoPagina(language)
+}
 
-function entrar(){ img.src = "img/crypto-anim.gif" }
+function preencherTextoPagina(language) {
+    preencherTextoHeader(language)
+    preencherTextoHome(language)
+    preencherTextoSobre(language)
+    preencherTextoProjetos(language)
+    preencherTextoContato(language)
+    preencherTextoFooter(language)
+}
 
-function sair(){ img.src = "img/crypto.png" }
+function preencherTextoHeader(language) {
+    if(language == 'pt-PT'){
+        document.querySelector('#header_inicio').innerHTML = 'Inicio'
+        document.querySelector('#header_sobre').innerHTML = 'Sobre'
+        document.querySelector('#header_projetos').innerHTML = 'Projetos'
+        document.querySelector('#header_contato').innerHTML = 'Contato'
+        return
+    }
+
+    document.querySelector('#header_inicio').innerHTML = 'Home'
+    document.querySelector('#header_sobre').innerHTML = 'About'
+    document.querySelector('#header_projetos').innerHTML = 'Projects'
+    document.querySelector('#header_contato').innerHTML = 'Contact'
+}
+
+function preencherTextoHome(language) {
+
+}
+
+function preencherTextoSobre(language) {
+
+}
+
+function preencherTextoProjetos(language) {
+
+}
+
+function preencherTextoContato(language) {
+
+}
+
+function preencherTextoFooter(language) {
+
+}
